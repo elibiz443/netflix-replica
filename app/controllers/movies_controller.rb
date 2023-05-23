4da22e18188ea94b601748cda65627f3139893e3
tuta2 <reyreghan20@gmail.com>
2023-05-23 23:02:12 +0300

    added search functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,17 @@
 class MoviesController < ApplicationController
   def index
-    @pagy_a, @movies = pagy_array(MovieService.new.fetch_movies, items: 6)
+    our_movies = MovieService.new.fetch_movies
+    @pagy_a, @movies = pagy_array(our_movies, items: 6)
+
+    @search = params['search']
+    if @search.present?
+      @title = @search['title']
+
+      if @title.present?
+        @movies = our_movies.where("title ILIKE ?", "%#{@title}%")
+      else
+        @movies = our_movies.where(title: @title)
+      end
+    end
   end
 end
