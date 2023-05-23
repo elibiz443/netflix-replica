75b7ee99284932b70d7e4f11ab9da7ad4b658c05
elibiz443 <ellyambet10@gmail.com>
2023-05-24 00:21:55 +0300

    corrected search functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,9 +8,18 @@ class MoviesController < ApplicationController
       @title = @search['title']
 
       if @title.present?
-        @movies = our_movies.where("title ILIKE ?", "%#{@title}%")
+        @movies = our_movies.select { |movie| movie["title"].downcase.include?(@title.downcase) }
+        if @movies.present?
+          flash[:notice] = "Below are your movies"
+          # redirect_to movies_path
+        else
+          flash[:warning] = "The movie doesn't exist!"
+          # redirect_to movies_path
+        end
       else
-        @movies = our_movies.where(title: @title)
+        @movies = our_movies.select { |movie| movie["title"] == @title }
+        flash[:alert] = "You provided null value!"
+        # redirect_to movies_path
       end
     end
   end
